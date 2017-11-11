da0ed70abca2fdc8450e13318e3526a6a8e4c0a1
Dipen Ved <vedipen@gmail.com>
2017-11-11 06:30:32 +0530

    Add comments to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
