8270dd7a2d952e3c35cba08995a9fa1597ea34cf
Dipen Ved <vedipen@gmail.com>
2017-11-13 05:55:10 +0530

    Remove migration with typo

--- db/migrate/20171110233135_add_user_id_to_post.rb
+++ db/migrate/20171110233135_add_user_id_to_post.rb
@@ -1,5 +1,5 @@
 class AddUserIdToPost < ActiveRecord::Migration[5.1]
   def change
-    add_column :posts, :user_od, :integer
+    add_column :posts, :user_id, :integer
   end
 end
--- db/migrate/20171110234101_change_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :posts, :user_od, :user_id
-  end
-end
