4b41474f2c2451b0c99c0c92ebd1ec5a395f2ea7
Dipen Ved <vedipen@gmail.com>
2017-11-18 00:24:16 +0530

    CSRF tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery  with: :null_session, only: Proc.new { |c| c.request.format.json? }
-# with: :exception, prepend: true
+  protect_from_forgery with: :exception, prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
