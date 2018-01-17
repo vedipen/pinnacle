8621284aa06f17c88f70dfad33f4cbce423b0ea2
Dipen Ved <vedipen@gmail.com>
2018-01-17 16:01:23 +0530

    Correct tests

--- test/controllers/hiddenleaderboard_controller_test.rb
+++ test/controllers/hiddenleaderboard_controller_test.rb
@@ -4,6 +4,6 @@ class HiddenleaderboardControllerTest < ActionDispatch::IntegrationTest
   # test "should get index" do
     # get final_leaderboard_url
     # assert_response :success
-  end
+  # end
 
 end
--- test/controllers/leaderboard_controller_test.rb
+++ test/controllers/leaderboard_controller_test.rb
@@ -4,6 +4,6 @@ class LeaderboardControllerTest < ActionDispatch::IntegrationTest
   # test "should get index" do
     # get originalscoreboard_url
     # assert_response :success
-  end
+  # end
 
 end
